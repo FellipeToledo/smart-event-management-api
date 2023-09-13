2f69f2dd285d819e54b7e3ec144587d0f9218118
FellipeToledo <fellipetoledo.dev@gmail.com>
2023-09-13 13:13:07 -0300

    implemented method sendVerificationEmail

--- auth/src/main/java/com/fajtech/auth/event/listener/RegistrationCompleteEventListener.java
+++ auth/src/main/java/com/fajtech/auth/event/listener/RegistrationCompleteEventListener.java
@@ -4,12 +4,17 @@ import com.fajtech.auth.event.RegistrationCompleteEvent;
 import com.fajtech.auth.User.User;
 import com.fajtech.auth.User.UserService;
 
+import jakarta.mail.MessagingException;
+import jakarta.mail.internet.MimeMessage;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
 import org.springframework.context.ApplicationListener;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Component;
 
+import java.io.UnsupportedEncodingException;
 import java.util.UUID;
 
 /**
@@ -21,6 +26,7 @@ import java.util.UUID;
 public class RegistrationCompleteEventListener implements ApplicationListener<RegistrationCompleteEvent> {
 
     private final UserService userService;
+    private final JavaMailSender mailSender;
     private  User theUser;
     @Override
     public void onApplicationEvent(RegistrationCompleteEvent event) {
@@ -40,8 +46,27 @@ public class RegistrationCompleteEventListener implements ApplicationListener<Re
         String url = event.getApplicationUrl()+"/api/v1/register/verifyEmail?token="+verificationToken;
 
         // Send the e-mail
-
+        try {
+            sendVerificationEmail(url);
+        } catch (MessagingException | UnsupportedEncodingException e) {
+            throw new RuntimeException(e);
+        }
         log.info("Click the link to verify your registration : {}", url);
-
+    }
+    public void sendVerificationEmail (String url) throws MessagingException, UnsupportedEncodingException {
+        String subject = "Email Verification";
+        String senderName = "Smart Event Management";
+        String mailContent = "<p> Hi, "+ theUser.getFirstName()+ ", </p>"+
+                "<p>Thank you for registering with us, "+"" +
+                "Please, follow the link below to complete your registration.</p>"+
+                "<a href=\"" +url+ "\">Verify your email to activate your account</a>"+
+                "<p> Thank you <br> Smart Event Management";
+        MimeMessage message = mailSender.createMimeMessage();
+        var messageHelper = new MimeMessageHelper(message);
+        messageHelper.setFrom("smarteventmanagement@gmail.com", senderName);
+        messageHelper.setTo(theUser.getEmail());
+        messageHelper.setSubject(subject);
+        messageHelper.setText(mailContent, true);
+        mailSender.send(message);
     }
 }
